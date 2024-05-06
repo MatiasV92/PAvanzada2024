39852c048834987104e37fadb619cc1bc720bfbd
MatiasV92 <matiasvicharelli.92@gmail.com>
2024-05-06 15:57:35 -0300

    Actualiazción P5 y P6

---
+++ Practico_7.hs
@@ -0,0 +1,39 @@
+--Ej1
+
+nand :: Bool -> Bool -> Bool
+nand True True = False
+nand _ _ = True
+
+--Ej2
+
+maj :: Bool -> Bool -> Bool -> Bool
+maj p q r | nand p q == False = True
+          | nand p r == False = True
+          | nand q r == False = True
+          | otherwise = False
+
+--Ej3
+
+existe :: (a -> Bool) ->[a] -> Bool
+existe f xs = or [ f x | x <- xs]
+
+paraTodo :: (a -> Bool) ->[a] -> Bool
+paraTodo f xs = and [ f x | x <- xs]
+
+-- xs_i EN HASKELL x!!i
+-- < (cuantif) i : R : T> = cuantif [ T | R ]
+
+--Ej4
+
+sumatoria :: (a -> Int) -> [Int] -> Int
+sumatoria f xs = sum [ f x | x <- xs]
+
+contaroria :: (a -> Int) -> [a] -> Int
+contaroria f xs = prod [ f x | x <- xs]
+
+productoria :: (a -> Int) ->[a] -> Int
+productoria f xs = [ f x | x <- xs]
+
+
+
+
